5740e94ae1eabe7b902508dc913488ebac1b1c8d
Irenia <15000616693@163.com>
2020-12-02 21:40:04 +0800

    demo

--- animated-banner.js
+++ animated-banner.js
@@ -58,11 +58,11 @@ async function blink () {
     // console.log(img.src)
     const blinkImges = ["./src/bilibili_1_1.png", "./src/bilibili_1_2.png","./src/bilibili_1_0.png"]
     
-    await sleep(50)
-    img.src = blinkImges[0]
-    await sleep(50)
-    img.src = blinkImges[1]
     await sleep(250)
+    img.src = blinkImges[0]
+    await sleep(250)
+    img.src = blinkImges[1]
+    await sleep(350)
     img.src = blinkImges[2]
     setTimeout(blink, 5000)   
 }
